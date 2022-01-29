f4c09b977dad7406ae4f9ef4a9b6e7e917df567f
liam1977 <lianzuozheng@hotmail.com>
2022-01-29 19:22:31 +0800

    第一次提交

---
+++ TestModel/src/main/java/edu/javase.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2015-2020 EmoreTech Studio. All Rights Reserved.
+ * Use is subject to license terms.
+ * <p>
+ * 在此处填写文件说明
+ */
+package edu;
+
+/**
+ * @author lianzuozheng@hotmail.com
+ */
+public class javase {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
